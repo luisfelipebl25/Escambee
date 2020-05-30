f0ab4fd14fe7c3dc4f1f6e343be90be3644fc4cb
MxBromelia <ruanmacambira@gmail.com>
2020-05-29 23:25:46 -0300

    answer_proposal funcional

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,30 +5,26 @@ class UsersController < ApplicationController
 
   def add_to_wishlist
     current_user.wishlist.push @game
-  end 
+  end
 
   def remove_from_wishlist
     current_user.wishlist.delete @game
-  end 
+  end
 
   def add_to_ownlist
     current_user.ownlist.push @game
-  end 
+  end
 
   def remove_from_ownlist
     current_user.ownlist.delete @game
   end
 
   def answer_proposal
-    # params['answer'] == true ? proposal.answer(current_user, true) : proposal.answer(current_user, false)
-    if params['answer']
-      current_user.proposals(Proposal.all).answer(current_user, true)
-    else 
-      current_user.proposals(Proposal.all).answer(current_user, false)
-    end 
-  end 
+    Proposal.find(params['proposal_id'])
+            .answer(current_user, params['answer'] == 'true')
+  end
 
   def create_game(params)
     @game = Game.new(params) 
-  end 
-end 
+  end
+end
